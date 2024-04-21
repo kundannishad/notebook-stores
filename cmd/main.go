1f4b465631aa36cc4355dfde3d352d1bc137ef7f
kundankumar <kundancse38@gmail.com>
2024-04-21 19:24:13 +0530

    kc

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,18 @@ import (
 	"log"
 	"net/http"
 	bookRoutes "notebook-stores/pkg/routes/book"
+	userRoute "notebook-stores/pkg/routes/user"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	//config.DbConnect()
+
 	r := mux.NewRouter()
 	bookRoutes.RegisterBookStoreRoutes(r)
+	userRoute.RegisterUserRoute(r)
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:8000", r))
